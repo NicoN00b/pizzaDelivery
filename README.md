a82c75fd10c1385448e3e9afcb95f1053b77f0e6
nr <nicholas.raethke@gmail.com>
2017-07-15 15:59:26 -0700

    several attempts at fixing JS with no success.

---
+++ README.md
@@ -0,0 +1,40 @@
+# _{pizzaDelivery for Pizza the Hutt}_
+
+#### _A simple web form for ordering pizza online}, {7.15.17}_
+
+#### By _**{Nico}**_
+
+## Description
+
+_{The Pizza the Hutt website should allow users to order multiple pizzas, choosing various options, indicate whether they wish to pick it up or have it delivered, and enter their contact information for delivery and/or confirmation.}_
+
+## Setup/Installation Requirements
+
+* No requirement beyond a modern web browser.
+
+_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+
+## Specs
+  It can return delivery or takeout and assign value.
+  It can return size and assign value.
+  It can return cheese and assign value.
+  It can accept multiple toppings checkboxes and assign value.
+  It can accept and return user contact information.
+
+## Known Bugs
+
+_{No known bugs, but it is known that the page is not yet functional.  There is something wrong with the object construction and event listener.  The form will not return the Pizza object nor the value of checked items.}_
+
+## Support and contact details
+
+_{contributions to this code could be beneficial, who knows if I'll figure it out. nicholas.raethke@gmail.com}_
+
+## Technologies Used
+
+_{This was created with JS, CSS, and HTML on Atom text editor with some help from git bash and links to JQuery-3.2.1 and Bootstrap.css.  I also used JSHint.com to help debug.}_
+
+### License
+
+*{Consider this Creative Commons, Open Source Code}*
+
+Copyright (c) 2017 **_{Nico, Epicodus Course Work}_**
--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,8 @@ Pizza.prototype.price = function() {
 
 Pizza.prototype.yourPizza = function() {
   var yourPizza = this.method + this.size + this.cheese + this.crust + this.toppings;
+  return yourPizza;
+
 };
 
 function resetFields() {
@@ -24,40 +26,32 @@ function resetFields() {
     $("input:checkbox[name='toppings']:checked").val("");
 }
 
-function Contact(first, last) {
+function Contact(first, last, address, phone, email) {
     this.firstName = first;
     this.lastName = last;
-    this.address = [];
+    this.address = [street, city, state, zip];
     this.phone = phone;
     this.email = email;
 }
 
-function Address(street, city, state, zip) {
-    this.street = street;
-    this.city = city;
-    this.state = state;
-}
 
-Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName + "<br>" + this.phone + "<br>" + this.email
+Contact.prototype.inputDetails = function() {
+    return this.firstName + " " + this.lastName + "<br>" + this.phone + "<br>" + this.email + "<br>" + this.address;
 };
 
-Address.prototype.fullAddress = function() {
-    return this.street + " " + this.city + ", " + this.state + " " + this.zip;
-}
 
 //Turn into Pizza Creation Event
 $(document).ready(function() {
 
   $("#add-pie").click(function() {
-    // $("#new-pie").append(pizzas)
-      //event.preventDefault();
+    $("#new-pie").append(pizzas)
+      event.preventDefault();
 
       var inputtedMethod = parseInt($("input:radio[name='method']:checked").val());
       var inputtedSize = parseInt($("select#size").val());
       var inputtedCheese = parseInt($("select#cheese").val());
       var inputtedCrust = parseInt($("select#crust").val());
-      var inputtedToppings = parseInt($("input:checkbox[name=toppings]:checked")).map(function() {
+      var inputtedToppings = parseInt($("input:checkbox[name=toppings]:checked")).each(function() {
         return (this).val();
       }).get();
 
@@ -67,25 +61,27 @@ $(document).ready(function() {
 
       console.log(newPizza);
 
-      $("#pizzas").toggle();
-      $(".method").text(inputtedMethod);
-      $(".size").text(inputtedSize);
-      $(".cheese").text(inputtedCheese);
-      $(".crust").text(inputtedCrust);
-      $(".toppings").text(inputtedToppings);
-      $(".price").text("$" + newPizza.price(price) + ".00");
+      $("#pizzas").show(Pizza);
+      $(".method").text(yourPizza.method);
+      $(".size").text(yourPizza.size);
+      $(".cheese").text(yourPizza.cheese);
+      $(".crust").text(yourPizza.crust);
+      $(".toppings").text(yourPizza.toppings);
+      $(".price").text("$" + Pizza.price() + ".00");
 
       resetFields();
 
-      // $(".new-address").each(function() {
-      //   var inputtedStreet = $(this).find("input.new-street").val();
-      //   var inputtedCity = $(this).find("input.new-city").val();
-      //   var inputtedState = $(this).find("input.new-state").val();
-      //   var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
-      //   newPizza.addresses.push(newAddress);
-      // });
-
-  // $('ul.pizzas').append("<li><span class='.yourPizzas'>" + newPizza.fullName() + "</span></li>");
-
   });
+
+    $(".new-contact").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedZip);
+      newPizza.addresses.push(newAddress);
+    });
+
+
+
+
 });
